a9bde145721f09687f6deed3712d5b5e798c9d97
Andrii Provozin <54965144+Developik@users.noreply.github.com>
2020-08-19 16:40:48 -0500

    Goal Scheduler
    A React-Native app made with the help of Expo while following Maximilian Schwarzmüller's lectures

---
+++ goals_scheduler/components/GoalInput.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
+
+const GoalInput = props => {
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    }
+
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    }
+
+    return (
+        <Modal visible={props.visible}
+            animationType='slide'
+            style={styles.modalStyle}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Course Goal"
+                    style={styles.input}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal} />
+                <View style={styles.buttonStyle}>
+                    <View style = {styles.button}>
+                        <Button title="CANCEL" color="red"
+                            onPress={props.onCancel} />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button title="ADD"
+                            onPress={addGoalHandler} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        width:'40%'
+    },
+    buttonStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '60%'
+    },
+    modalStyle: {
+        borderWidth: 0,
+        borderColor: 'transparent'
+    },
+    inputContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 10
+    }
+}
+)
+
+export default GoalInput;
